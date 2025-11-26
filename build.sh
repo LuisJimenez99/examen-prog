574866900c3d7acc215ca639daaee3a57da1d411
Luis Jimenez <jimenezlg.99@gmail.com>
2025-11-26 15:05:01 -0300

    Forzando instalacion de Gunicorn en build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+
+# --- TRUCO: Instalar Gunicorn explícitamente primero ---
+pip install gunicorn
+
+# Instalar el resto de librerías
+pip install -r requirements.txt
+
+# Recolectar archivos estáticos
+python manage.py collectstatic --no-input
+
+# Ejecutar migraciones
+python manage.py migrate
