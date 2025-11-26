2cb03ab632a5ead8531d16a9fd85a3ec44790207
Luis Jimenez <jimenezlg.99@gmail.com>
2025-11-26 15:38:52 -0300

    agregando email

--- core/settings.py
+++ core/settings.py
@@ -93,7 +93,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 if not DEBUG:
-    
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 else:
@@ -109,14 +108,19 @@ LOGIN_URL = 'login'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') 
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+EMAIL_HOST_USER = 'apikey'
 
 
-if not EMAIL_HOST_USER or not EMAIL_HOST_PASSWORD:
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+
+
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_FROM', 'jimenezlg.99@gmail.com')
+
+
+if not EMAIL_HOST_PASSWORD:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    EMAIL_HOST_USER = 'test@localhost'
